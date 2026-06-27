fbce94873e731027429cc6470b8927c963d7df11
Dmytro Mariukhna <163506903+gitCode5345@users.noreply.github.com>
2026-06-27 13:03:33 +0300

    KAN-31: added animation bars when clicker is working (#21)
    * KAN-31: added animation bars when clicker is working
    
    * KAN-31: added indentation
    
    * KAN-31: remove not used constant and added const for a formula

--- lib/src/core/constants/dimensions.dart
+++ lib/src/core/constants/dimensions.dart
@@ -1,3 +1,5 @@
 const double smallDesktopWidth = 680.0;
 const double sideBarWidth = 250.0;
 const double mainContentMaxWidth = 1200.0;
+const double maxBarHeight = 60.0;
+const double minBarHeight = 4.0;
